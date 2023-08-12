4b936fe304205d61e889a57684c116f7692b04cd
Your Name <you@example.com>
2023-08-12 14:02:38 -0700

    new todo form component created

---
+++ todos/src/NewTodoForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+function NewTodoForm({ createTodo }) {
+    const [task, setTask] = useState("");
+
+    const handleChange = e => {
+        setTask(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        createTodo({
+            task,
+            id: uuid()
+        });
+        setTask("");
+    };
+
+    return (
+        <div>        
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="task">New Todo</label>
+            <input
+                type="text"
+                placeholder="New Todo"
+                id="task"
+                name="task"
+                value={task}
+                onChange={handleChange}
+            />
+            <button>Add Todo</button>
+        </form>
+        </div>
+    );
+}
